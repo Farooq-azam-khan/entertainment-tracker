6227c98309c692c132019fb3a2c56002fcfe1295
Farooq Azam Khan <khanazamfarooq99@gmail.com>
2020-12-14 19:39:37 -0500

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: {
+    enabled: false, 
+    content: ['./src/**/*.html', './src/**/*.js', './src/**/*.ts', './src/**/*.elm']
+  },
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
