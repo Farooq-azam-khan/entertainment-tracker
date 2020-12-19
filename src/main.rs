bfd2548a9cde3f622032cb711af3021681aa72be
Farooq Azam Khan <khanazamfarooq99@gmail.com>
2020-12-19 16:53:43 -0500

    experiment with dotenv is a success

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
+
+extern crate dotenv;
+
+use dotenv::dotenv;
+use std::env;
+
+#[get("/my-secret")]
+fn get_secret() -> String {
+        String::from(env::var("MY_SECRET").unwrap().as_str())
+}
 
 #[get("/")]
 fn index() -> &'static str {
@@ -8,5 +19,8 @@ fn index() -> &'static str {
 }
 
 fn main() {
-        rocket::ignite().mount("/", routes![index]).launch();
+        dotenv().ok();
+        rocket::ignite()
+                .mount("/", routes![index, get_secret])
+                .launch();
 }
