61f60a835cab99e79bfcc2fb7789fb8a81c73b51
Farooq Azam Khan <khanazamfarooq99@gmail.com>
2020-12-20 12:01:18 -0500

    parse page

--- src/Route.elm
+++ src/Route.elm
@@ -1,17 +1,19 @@
 module Route exposing (Route(..), parseUrl)
 
 import Url exposing (Url)
-import Url.Parser exposing (..)
+import Url.Parser as Parser exposing ((</>), Parser)
 
 
 type Route
     = NotFound
-    | Login
+    | Login String
+    | LoginPage
+    | HomePage
 
 
 parseUrl : Url -> Route
 parseUrl url =
-    case parse matchRoute url of
+    case Parser.parse matchRoute url of
         Just route ->
             route
 
@@ -19,6 +21,15 @@ parseUrl url =
             NotFound
 
 
+urlHead : String
+urlHead =
+    "Main.elm"
+
+
 matchRoute : Parser (Route -> a) a
 matchRoute =
-    oneOf [ map Login (s "login") ]
+    Parser.oneOf
+        [ Parser.map HomePage (Parser.s urlHead)
+        , Parser.map Login (Parser.s urlHead </> Parser.s "login" </> Parser.string)
+        , Parser.map LoginPage (Parser.s urlHead </> Parser.s "login")
+        ]
