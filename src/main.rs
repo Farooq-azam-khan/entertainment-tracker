cc42bc44eee7e977d00616dd37d33afc6fd01727
Farooq Azam Khan <khanazamfarooq99@gmail.com>
2020-12-20 21:07:50 -0500

    get json data and serialize it

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,10 @@
 #[macro_use]
 extern crate rocket;
 use postgres::{Client, Error, NoTls};
+use reqwest::header::USER_AGENT;
+use rocket::response::content;
 use rocket::response::Redirect;
+use serde::{Deserialize, Serialize};
 
 extern crate dotenv;
 
@@ -53,6 +56,48 @@ fn get_access_token(code: String) -> Option<String> {
         }
 }
 
+#[derive(Deserialize, Serialize, Debug)]
+struct User {
+        name: String,
+        email: Option<String>,
+}
+
+fn query_github(token: String) -> Option<User> {
+        let user_info_url = "https://api.github.com/user";
+        let header = format!("token {}", token);
+        let client = reqwest::blocking::Client::new();
+        let res = client
+                .get(user_info_url)
+                .header("Authorization", header.as_str())
+                .header(USER_AGENT, " Mozilla/5.0")
+                .send();
+
+        match res {
+                Ok(response) => {
+                        // match
+                        let json_user: User =
+                                serde_json::from_str(response.text().unwrap().as_str()).unwrap();
+                        println!("{:?}", json_user);
+                        Some(json_user)
+                }
+                Err(_) => None,
+        }
+}
+
+#[get("/user/<access_token>")]
+fn get_user_information(access_token: Option<String>) -> content::Json<String> {
+        match access_token {
+                Some(token) => {
+                        // fetch user
+                        let user = query_github(token).unwrap();
+                        // TODO: check database for user
+
+                        content::Json(serde_json::to_string(&user).unwrap())
+                }
+                None => content::Json(String::from("None")),
+        }
+}
+
 #[get("/login/github/callback?<code>")]
 fn github_callback(code: Option<String>) -> Redirect {
         match code {
@@ -60,7 +105,6 @@ fn github_callback(code: Option<String>) -> Redirect {
                         let access_token = get_access_token(token);
                         match access_token {
                                 Some(at) => {
-                                        // let jsonToken = content::Json(at);
                                         let redirect_to_frontend =
                                                 format!("http://localhost:8000/login?{}", at);
                                         println!("sending back to {}", redirect_to_frontend);
@@ -108,7 +152,13 @@ fn main() {
         rocket::ignite()
                 .mount(
                         "/",
-                        routes![index, get_secret, github_oauth, github_callback,],
+                        routes![
+                                index,
+                                get_secret,
+                                github_oauth,
+                                github_callback,
+                                get_user_information
+                        ],
                 )
                 .launch();
 }
