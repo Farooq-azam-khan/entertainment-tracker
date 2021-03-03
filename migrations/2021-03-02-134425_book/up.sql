cccc00e2f5239267d8d8140297355fd3f612ae2f
Farooq Azam Khan <khanazamfarooq99@gmail.com>
2021-03-03 10:58:47 -0500

    added total pages and total chapter columns for book table

--- migrations/2021-03-02-134425_book/up.sql
+++ migrations/2021-03-02-134425_book/up.sql
@@ -2,5 +2,7 @@ CREATE TABLE book (
     id SERIAL PRIMARY KEY, 
     title VARCHAR NOT NULL,
     author_id INT REFERENCES author (id) ON DELETE SET NULL, 
+    total_pages INT NOT NULL DEFAULT 1, 
+    total_chapters INT NOT NULL DEFAULT 1,
     CHECK (title <> '')
 ); 
