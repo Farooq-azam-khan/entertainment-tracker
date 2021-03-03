ec5dbb209b20c4be1ba21e7ec497a64f548faf6f
Farooq Azam Khan <khanazamfarooq99@gmail.com>
2021-03-03 10:59:22 -0500

    Update schema.rs

--- src/schema.rs
+++ src/schema.rs
@@ -10,12 +10,25 @@ table! {
         id -> Int4,
         title -> Varchar,
         author_id -> Nullable<Int4>,
+        total_pages -> Int4,
+        total_chapters -> Int4,
+    }
+}
+
+table! {
+    reading_history (id) {
+        id -> Int4,
+        book_id -> Nullable<Int4>,
+        end_page -> Int4,
+        created_at -> Timestamp,
     }
 }
 
 joinable!(book -> author (author_id));
+joinable!(reading_history -> book (book_id));
 
 allow_tables_to_appear_in_same_query!(
     author,
     book,
+    reading_history,
 );
